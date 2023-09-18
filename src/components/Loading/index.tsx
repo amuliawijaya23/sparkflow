3f022ee2e84a3d34ed111bc78961c05b712838ad
amuliawijaya23 <amuliawijaya@gmail.com>
2023-09-18 22:59:19 +0700

    updates loading component sx/css

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -3,8 +3,14 @@ import { Box, CircularProgress } from '@mui/material';
 
 export const Loading = () => {
   return (
-    <Box sx={{ height: '100%' }}>
-      <CircularProgress />
+    <Box
+      sx={{
+        height: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <CircularProgress size={'4.5rem'} />
     </Box>
   );
 };
